15ef71882abcd29203d36b88af571b3547f17396
yt-koike <y.koike.2001code@gmail.com>
2024-05-29 17:01:21 +0900

    implement CheckBoxGrid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,74 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+'use client';
+import { Dispatch, SetStateAction, useState } from "react";
+
+class CheckBox {
+  value: string;
+  title: string;
+  isChecked: boolean;
+  setChecked: Dispatch<SetStateAction<boolean>>;
+  handler: () => void;
+  constructor(value: string = "") {
+    this.value = value;
+    const [title, setTitle] = useState("");
+    this.title = title;
+    const [isChecked, setChecked] = useState(false);
+    this.isChecked = isChecked;
+    this.setChecked = setChecked;
+    this.handler = () => {setTitle("pushed")};
+  }
+  getTitle(title:string){
+   return  this.title;
+  }
+  setTitle(title:string){
+    this.title=title;
+  }
+  toHtml() {
+    return (<><input type="checkbox" checked={this.isChecked} onChange={(e)=>{this.setChecked(!this.isChecked);this.handler();}}></input>{this.title}</>);
+  }
+}
+
+class CheckBoxGrid {
+  w: number;
+  h: number;
+  checkBoxes: CheckBox[];
+  constructor(w: number, h: number) {
+    this.w = w;
+    this.h = h;
+    this.checkBoxes = [];
+    for (var i = 0; i < h; i++) {
+      for (var j = 0; j < w; j++) {
+        this.checkBoxes.push(new CheckBox("1"));
+      }
+    }
+  }
+  get(i: number, j: number): CheckBox {
+    return this.checkBoxes[i * this.w + j];
+  }
+  set(i: number, j: number, checkBox: CheckBox): void {
+    this.checkBoxes[i * this.w + j] = checkBox;
+  }
+  toHtml(): JSX.Element {
+    var tr = [];
+    for (var i = 0; i < this.h; i++) {
+      var td = [];
+      for (var j = 0; j < this.w; j++) {
+        td.push(<td>{this.checkBoxes[i * this.w + j].toHtml()}</td>);
+      }
+      tr.push((<tr>{td}</tr>));
+    }
+    return (
+      <table>
+        <tbody>
+         {tr}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  const checkBoxGrid = new CheckBoxGrid(5, 5);
+  console.log(1);
+  return <div>{checkBoxGrid.toHtml()}</div>;
 }
